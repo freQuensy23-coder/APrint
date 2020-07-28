00ae48fbe8bae1eba99fde5f6a88cb8af8bed80a
freQuensy <mametevalex@gmail.com>
2020-07-29 00:00:00 +0300

    Added tests

---
+++ tests.py
@@ -0,0 +1,12 @@
+# Scanner test
+import glob
+from Scanner import *
+
+PDF_Folder = "PDFs/*.pdf"
+paths = (glob.glob(PDF_Folder))
+print("Found {} PDFs".format(str(len(paths))))
+
+with open("res.txt", "a") as res:
+    for path in paths:
+        scanner = Scanner(path)
+        res.write(str(scanner))
