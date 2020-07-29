802ea861f802f01cb9146376931dbdb60c9175b9
freQuensy <mametevalex@gmail.com>
2020-07-29 12:28:29 +0300

    Added init in QR class

--- QR.py
+++ QR.py
@@ -2,5 +2,10 @@ import qrcode
 import private.config
 
 class QR:
-    def __init__(self, cost, name, address, ):
-        pass
+    """Генерирует QR код. need_divide_name - нужно ли разделять имя на имя фамилию и отчество методом .split()"""
+    def __init__(self, cost, name, address, period, need_divide_name=True):
+        self.cost = cost
+        self.name = name
+        self.address = address
+        self.period = period
+        self.settings = {"need_divide_name":need_divide_name}
