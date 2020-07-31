d9dc18a6a64b536dbada09bd3c45600d2525ffb0
Alexey <mametevalex@gmail.com>
2020-07-31 21:06:08 +0300

    Added qrgen.py

---
+++ qrgen.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('/home/alex/Documents/PythonProjects/APrint/')
+
+import glob
+from Classes.Scanner import *
+from Classes.QR import *
+from Classes.Stamper import *
+from Classes.Printer import *
+import pdf2image
+
