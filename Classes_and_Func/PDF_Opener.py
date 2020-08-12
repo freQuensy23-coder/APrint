45a5ba063f7d6b14c389cbd004dcf1029966c072
freQuensy <mametevalex@gmail.com>
2020-08-12 20:27:52 +0300

    Переделал функцию PDF open. Добавил туда получения текста страниц их содержимого

--- Classes_and_Func/PDF_Opener.py
+++ Classes_and_Func/PDF_Opener.py
@@ -5,9 +5,11 @@ def open_PDF(path):
     return fitz.open(path)
 
 
-def get_pdf_pages_text(fitz_PDF):
-    """Get all pages text (from fitz document)"""
+def get_pdf_pages(fitz_PDF):
+    """Get all pages and text (from fitz PDF document)"""
     result = []
+    pdf_pages = []
     for page in fitz_PDF:
         result.append(page.getText("text"))
-    return result
+        pdf_pages.append(page)
+    return {"pdf_text":result, "pdf_pages":pdf_pages}
