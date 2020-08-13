2cd42261cafbe93c1dc6125e932bc3b68d25bee3
freQuensy <mametevalex@gmail.com>
2020-08-13 14:28:52 +0300

    Now get time returns 12-00 instead of 12:00 because filenames can't consist any ':' char

--- Classes_and_Func/get_time.py
+++ Classes_and_Func/get_time.py
@@ -1,5 +1,5 @@
 import datetime
 
 def get_time():
-    # Returns smth. like this: "12:00" or "02:51"
-    return str(datetime.datetime.now().time())[0:5]
+    # Returns smth. like this: "12-00" or "02-51"
+    return str(str(datetime.datetime.now().time())[0:5]).replace(":", "-")
