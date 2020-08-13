e95d91251fbfb79b101888656674c65903f1d637
freQuensy <mametevalex@gmail.com>
2020-08-13 10:43:00 +0300

    Fixed bugs in get time .py

--- Classes_and_Func/get_time.py
+++ Classes_and_Func/get_time.py
@@ -1,6 +1,5 @@
 import datetime
 
-
 def get_time():
     # Returns smth. like this: "12:00" or "02:51"
-    return str(datetime.now().time())[0:5]
+    return str(datetime.datetime.now().time())[0:5]
