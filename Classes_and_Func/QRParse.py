d3196c3344cefe447af02e9cd9d7d93e42be6c7b
freQuensy <mametevalex@gmail.com>
2020-08-13 14:44:36 +0300

    Added new parser for single pdf documents

--- Classes_and_Func/QRParse.py
+++ Classes_and_Func/QRParse.py
@@ -6,3 +6,5 @@ parser.add_argument('folder_path', action = 'store', help = 'Path to folder with
 parser.add_argument("-s",action = 'store_true', dest= "sQR", help = "Сохранять картинки QR")
 parser.add_argument("-m",action = 'store_true', dest = "MergePDF", help = "Open image in default viewer")
 
+one_pdf_parser = argparse.ArgumentParser(description='Scan and generate QRs in one PDF')
+parser.add_argument('file_path', action = 'store', help = 'Path PDF file')
