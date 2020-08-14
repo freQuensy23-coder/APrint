37ead50852a201b040da6097a00923f5070fcce6
freQuensy <mametevalex@gmail.com>
2020-08-14 13:22:02 +0300

    Reformat OnePdfScanner. Created func for every basic event

--- OnePdfScanner.py
+++ OnePdfScanner.py
@@ -1,8 +1,4 @@
 import sys
-
-sys.path.append(r'C:\Users\mamet\PycharmProjects\QRgenerator')
-sys.path.append(r'C:\Users\mamet\PycharmProjects\QRgenerator\poppler')
-
 from Classes_and_Func.Scanner import *
 from Classes_and_Func.QR import *
 from Classes_and_Func.Stamper import *
@@ -20,12 +16,6 @@ import colorama
 import PyPDF2
 
 colorama.init()
-
-pdf_write_object = PyPDF2.PdfFileWriter()
-
-
-def get_file_path(args):
-    return sys.argv[1]
 
 
 def connect(str_list):
@@ -45,48 +35,62 @@ def get_file_name(filename):
     return res
 
 
-def stamp_pages(pdf_pages_images, pdf_pages_texts):
+def stamp_pages(pdf_pages_images, datas):
+    """datas -лист словариков с информацией, которую нужно добавить на i-ую платежку ( {"cost": self.cost,
+                     "bank_id": self.bank_id,
+                     "period": self.period,
+                     "name": self.name,
+                     "address": self.address})
+        Если на i-ую платежку не нужно добавлять картинку, то datas[i] = None
+                    """
     stamped_images = []
-    for page_number, page_text in enumerate(pdf_pages_texts):
-        try:
-            scanner = Scanner(page_text)  # raise NotDocumentError If page_number page does not contains receipt data
-            data = scanner.data
+    for page_number, image in enumerate(pdf_pages_images):
+        if datas[page_number] is not None:
+            data = datas[page_number]
             qr = QR(cost=data["cost"], name=data["name"], address=data["address"], period=data["period"])
             print(data["name"])
+            print("sslas")
             stamper = Stamper(qr=qr.qr, document_image=pdf_pages_images[page_number])
             stamper.do_stamp()
             stamped_images.append(stamper.document)
-        except NotDocumentError:
+        else:
             # If page_number page does not contains receipt data
             print("На странице {i} не найден платёжный документ.".format(i=page_number))
             stamped_images.append(pdf_pages_images[page_number])
-        except:
-            print(page_text)
-            print(0 / 0)
     return stamped_images
 
 
-def main(file_path):
-    global pdf_write_object
+def get_pages_data(pages_texts):
+    pages_data = []
 
-    # Get PIL.Image of every pdf page
-    pdf_pages_images = get_pdf_images(file_path)
+    def get_page_data(page_text):
+        """Возвращает словарик с информацией о платежке по данному тексту.
+        Если переданный текст не является платёжкой то вызывает NotDocumentError """
+        scanner = Scanner(page_text)  # raise NotDocumentError If page_number page does not contains receipt data
+        data = scanner.data
+        return data
 
-    # List where PIL.Images of payments will be placed after Stamper "stamp" them
-    stamped_pages_images = []
+    for page_num, page_text in enumerate(pages_texts):
+        try:
+            pages_data.append(get_page_data(page_text))
+        except NotDocumentError:
+            pages_data.append(None)
+    return pages_data
 
-    # get pages data
-    pages, pdf_pages_texts = get_pages_data(file_path)
+def save_images_to_pdf(images, filepath, quality):
+    """Save all PIL.Images to pdf file"""
+    images[0].save(filepath,
+                   resolution=100.0,
+                   save_all=True,
+                   append_images=images[1:],
+                   quality = quality
+                   )
 
 
-    stamped_pages_images = stamp_pages(pdf_pages_images, pdf_pages_texts)
-    # List of images: Pdf's pages with QR and warning text
-    print(stamped_pages_images)
-    img = stamped_pages_images[0]
-    img.save(get_file_name(file_path) + "_stamped_" + get_time() + ".pdf",
-             resolution=100.0,
-             save_all=True,
-             append_images=stamped_pages_images[1:])
+def get_pdf_file_path(source_file_path):
+    """Превращает путь до исходного файла в путь для конечного файла"""
+    return connect(source_file_path.split(".")[:-1]) + "_stamped-" + get_time() + ".pdf"
+
 
 
 def get_pdf_images(FILE_PATH):
@@ -94,15 +98,33 @@ def get_pdf_images(FILE_PATH):
     return pdf2image.convert_from_path(FILE_PATH)
 
 
-def get_pages_data(file_path):
+def get_pages_texts_docs(file_path):
     """Returns dict of lists with pages (fitz?) and their text"""
-    pages_data = namedtuple("data",  ["pages", "text"])
+    pages_data = namedtuple("data", ["pages", "text"])
     pages_info = get_pdf_pages(open_PDF(file_path))
     pages = pages_info["pdf_pages"]
     pdf_pages_texts = pages_info["pdf_text"]
     print("Найдено {num} листов".format(num=len(pdf_pages_texts)))
     result = pages_data(pages, pdf_pages_texts)
     return result
+
+
+def main(file_path):
+    # Get pages texts
+    _, pdf_pages_texts = get_pages_texts_docs(file_path)
+
+    # Get PIL.Image of every pdf page
+    pdf_pages_images = get_pdf_images(file_path)
+
+    # get pages data
+    pages_data = get_pages_data(pdf_pages_texts)
+
+    stamped_pages_images = stamp_pages(pdf_pages_images, pages_data)
+    # List of images: Pdf's pages with QR and warning text
+
+    result_filename = get_pdf_file_path(source_file_path=file_path)
+    save_images_to_pdf(stamped_pages_images, result_filename, quality=75)
+
 
 if __name__ == '__main__':
     args = one_pdf_parser.parse_args()
