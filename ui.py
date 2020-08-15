d609ea303a72f1c3140b2c3f54e7e6dd7bd81b51
seny <senymama228@gmail.com>
2020-08-15 18:27:01 +0300

    completed dropdown list

--- ui.py
+++ ui.py
@@ -2,7 +2,7 @@ from Classes_and_Func.PagesFunc import *
 from tkinter import *
 from tkinter.ttk import *
 
-pages_texts, images, pages_data = 0, 0, 0
+pages_texts, images, pages_data, insert_pages_data = 0, 0, 0, 0
 win = Tk()
 combo_var = StringVar()
 
@@ -18,21 +18,34 @@ def getting_data_from_pdf(file_path):
 
 def main(file_path):
     def combo_bind(event):
-        global combo_var
+        global combo_var, insert_pages_data
         print(combo_var.get())
+        for i, el in enumerate(insert_pages_data):
+            if el == combo_var.get():
+                print('Выбранн элемент {}'.format(i))
+
+    def pages_data_to_insert_format(pages_data):
+        tmp = []
+        for i, el in enumerate(pages_data):
+            if el is None:
+                pass
+            else:
+                a = el['cost']
+                sum = '{},{} Руб.'.format((int(a) - int(a) % 100) // 100, int(a) % 100)
+                tmp.append('{},   {}'.format(el['name'], sum))
+        return tmp
 
     getting_data_from_pdf(file_path)
-    global pages_data, pages_texts, images
+    global pages_data, pages_texts, images, insert_pages_data
     global win, combo_var
 
+    insert_pages_data = pages_data_to_insert_format(pages_data)
     win.geometry('500x500')
-    # c.set()
-    combo = Combobox(win, textvariable=combo_var)
-    combo.grid()
+    combo_var.set(insert_pages_data[1])
+    combo = Combobox(win, textvariable=combo_var, values=insert_pages_data, width=60)
+    combo.grid(row=0, columnspan=5, pady=3)
     combo.bind('<<ComboboxSelected>>', combo_bind)
-
     win.mainloop()
-
 
 
 if __name__ == '__main__':
