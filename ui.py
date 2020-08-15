c819a72419800f6c75c1698a4acaf7027bb4c921
seny <senymama228@gmail.com>
2020-08-15 16:23:39 +0300

    created function for getting data from pdf

--- ui.py
+++ ui.py
@@ -1,12 +1,21 @@
 from Classes_and_Func.PagesFunc import *
+pages_texts, images, pages_data = 0, 0, 0
 
 
-def main(file_path):
+def getting_data_from_pdf(file_path):
+    global pages_data, pages_texts, images
     _, pages_texts = get_pages_texts_docs(file_path)
-    
+
     images = get_pdf_images(file_path)
 
     pages_data = get_pages_data(pages_texts)
+
+
+
+def main(file_path):
+    getting_data_from_pdf(file_path)
+    global pages_data, pages_texts, images
+    
 
 
 
