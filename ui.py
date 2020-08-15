9c3723398aae587874d2b2a5df4c1f3aba792086
seny <senymama228@gmail.com>
2020-08-15 16:34:19 +0300

    first version of the gui

--- ui.py
+++ ui.py
@@ -1,5 +1,10 @@
 from Classes_and_Func.PagesFunc import *
+from tkinter import *
+from tkinter.ttk import *
+
 pages_texts, images, pages_data = 0, 0, 0
+win = Tk()
+combo_var = StringVar()
 
 
 def getting_data_from_pdf(file_path):
@@ -11,11 +16,22 @@ def getting_data_from_pdf(file_path):
     pages_data = get_pages_data(pages_texts)
 
 
-
 def main(file_path):
+    def combo_bind(event):
+        global combo_var
+        print(combo_var.get())
+
     getting_data_from_pdf(file_path)
     global pages_data, pages_texts, images
-    
+    global win, combo_var
+
+    win.geometry('500x500')
+    # c.set()
+    combo = Combobox(win, textvariable=combo_var)
+    combo.grid()
+    combo.bind('<<ComboboxSelected>>', combo_bind)
+
+    win.mainloop()
 
 
 
