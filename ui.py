d30f7720070e76d43088bc89a0166dba45abc007
seny <senymama228@gmail.com>
2020-08-17 15:49:39 +0300

    test case text_geter_in_entry

--- ui.py
+++ ui.py
@@ -4,8 +4,8 @@ from tkinter.ttk import *
 
 pages_texts, images, pages_data, insert_pages_data, my_pages_data = 0, 0, 0, 0, 0
 file_path = 0
-win = Tk()
-combo_var = StringVar()
+win = 0
+combo_var = 0
 ename, eperiod, eaddress, ebankid, ecost = Entry(width=40), Entry(width=40), Entry(width=40), Entry(width=40), \
                                            Entry(width=40)
 
@@ -22,12 +22,22 @@ def getting_data_from_pdf(file_path):
 def main(fp):
     global file_path
     file_path = fp
+    global win, combo_var
+    win = Tk()
+    combo_var = StringVar()
 
     def text_geter_in_entry(page):
         def geter():
-            global ename, eperiod, eaddress, ebankid, ecost, my_pages_data
-            mas = [ename.get(), eperiod.get(), eaddress.get(), ebankid.get(), int(float(ecost.get().split()[0]) * 100)]
-            return
+            dic = {}
+            global ename, eperiod, eaddress, ecost, my_pages_data
+            try:
+                dic['cost'] = int(float(ecost.get().split()[0]) * 100)
+                dic['period'] = eperiod.get()
+                dic['name'] = ename.get()
+                dic['address'] = eaddress.get()
+                return dic
+            except IndexError:
+                return 0
 
         global my_pages_data
         tmp_page_data = []
@@ -35,42 +45,45 @@ def main(fp):
             if i != page:
                 tmp_page_data.append(my_pages_data[i])
             elif i == page:
-                geter()
+                if geter():
+                    tmp_page_data.append(geter())
+        print(tmp_page_data)
 
     def text_inserter_to_entry(page):
         global pages_data
 
-        def recorder(name, period, adres, bankid, cost):
-            global ename, eperiod, eaddress, ebankid, ecost
+        def recorder(name, period, adres, cost):
+            global ename, eperiod, eaddress, ecost
             ename.insert(0, name)
             eperiod.insert(0, period)
             eaddress.insert(0, adres)
-            ebankid.insert(0, bankid)
             ecost.insert(0, cost)
 
         def cleaner():
-            global ename, eperiod, eaddress, ebankid, ecost
+            global ename, eperiod, eaddress, ecost
             ename.delete(0, END)
             eperiod.delete(0, END)
             eaddress.delete(0, END)
-            ebankid.delete(0, END)
             ecost.delete(0, END)
 
         pd = pages_data[page]
         name = pd['name']
         period = pd['period']
         adress = pd['address']
-        bank = pd['bank_id']
         a = pd['cost']
         cost = '{}.{} Руб.'.format((int(a) - int(a) % 100) // 100, int(a) % 100)
         cleaner()
-        recorder(name, period, adress, bank, cost)
+        recorder(name, period, adress, cost)
 
     def ok_bind():
         global images, pages_data, file_path
         stamped_images = stamp_pages(images, pages_data)
         final_file_path = get_pdf_file_path(file_path)
         save_images_to_pdf(stamped_images, final_file_path)
+
+    def defult_bind():
+        my_pages_data = pages_data
+        combo_bind(0)
 
     def combo_bind(event):
         global combo_var, pages_data
@@ -104,24 +117,21 @@ def main(fp):
     combo = Combobox(win, textvariable=combo_var, values=insert_pages_data, width=70)
     combo.grid(row=0, columnspan=5, pady=3)
     combo.bind('<<ComboboxSelected>>', combo_bind)
-    global ename, eperiod, eaddress, ebankid, ecost
+    global ename, eperiod, eaddress, ecost
     ename.grid(row=1, column=1)
     Label(text='ФИО').grid(row=1, column=0)
     eperiod.grid(row=2, column=1)
     Label(text='Период').grid(row=2, column=0)
     eaddress.grid(row=3, column=1)
     Label(text='Адресат').grid(row=3, column=0)
-    ebankid.grid(row=4, column=1)
-    Label(text='Банк').grid(row=4, column=0)
     ecost.grid(row=5, column=1)
-    Label(text='Сумма').grid(row=5, column=0)
+    Label(text='Сумма').grid(row=4, column=0)
     bOK = Button(text='OK')
-    bOK.grid(row=6, column=4)
+    bOK.grid(row=5, column=4, command=ok_bind)
     bDefult = Button(text='По Умолчанию')
-    bDefult.grid(row=6, column=3)
+    bDefult.grid(row=5, column=3)
     win.mainloop()
 
 
 if __name__ == '__main__':
-    # main("test.pdf")
-    getting_data_from_pdf('test.pdf')
+    main("test.pdf")
