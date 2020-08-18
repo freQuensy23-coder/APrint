aa948d16ff33cbff3155abc52dfeebaf6025e3b7
seny <senymama228@gmail.com>
2020-08-18 13:23:52 +0300

    code is commented out

--- ui.py
+++ ui.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter.ttk import *
 
 pages_texts, images, pages_data, insert_pages_data, my_pages_data = 0, 0, 0, 0, 0
+user_num = None
 file_path = 0
 win = Tk()
 combo_var = StringVar()
@@ -20,28 +21,27 @@ def getting_data_from_pdf(file_path):
 
 
 def main(fp):
-    global file_path
+    global file_path, win, combo_var
     file_path = fp
-    global win, combo_var
 
     def text_geter_in_entry(page):
+        """Change temp page data varible, after geting new data from entry"""
         def geter():
+            """Get data from entry (input text)"""
             dic = {}
             global ename, eperiod, eaddress, ecost, my_pages_data
-            # try:
-            if 1:
-                a = ecost.get()
-                if a != '':
-                    dic['cost'] = int(float(ecost.get().split()[0]) * 100)
-                    dic['period'] = eperiod.get()
-                    dic['name'] = ename.get()
-                    dic['address'] = eaddress.get()
-                    return dic
-                else:
-            # except IndexError:
-                    return 0
+            a = ecost.get()
+            if ecost.get() != '':
+                dic['cost'] = int(float(ecost.get().split()[0]) * 100)
+                dic['period'] = eperiod.get()
+                dic['name'] = ename.get()
+                dic['address'] = eaddress.get()
+                return dic
+            else:
+                return 0
 
         global my_pages_data
+        # Обнроляем масси временной информации о людях
         tmp_page_data = []
         for i, el in enumerate(my_pages_data):
             if i != page:
@@ -49,14 +49,16 @@ def main(fp):
             elif i == page:
                 if geter() != 0:
                     tmp_page_data.append(geter())
-                elif geter() == 0:
+                else:
                     tmp_page_data.append(my_pages_data[i])
         print(tmp_page_data)
 
     def text_inserter_to_entry(page):
+        """Change entry text after person selected"""
         global pages_data
 
         def recorder(name, period, adres, cost):
+            """iNSERT TEXT TO ENTRY"""
             global ename, eperiod, eaddress, ecost
             ename.insert(0, name)
             eperiod.insert(0, period)
@@ -64,6 +66,7 @@ def main(fp):
             ecost.insert(0, cost)
 
         def cleaner():
+            """clean entrys old text"""
             global ename, eperiod, eaddress, ecost
             ename.delete(0, END)
             eperiod.delete(0, END)
@@ -80,30 +83,42 @@ def main(fp):
         recorder(name, period, adress, cost)
 
     def ok_bind():
+        """DO this after OK clicked
+            -Get filename
+            -Save file (PDF)
+        """
         global images, pages_data, file_path
         stamped_images = stamp_pages(images, pages_data)
         final_file_path = get_pdf_file_path(file_path)
         save_images_to_pdf(stamped_images, final_file_path)
 
     def defult_bind():
+        """Do this after default clicked
+        Reset data to defaults"""
         global my_pages_data
         my_pages_data = pages_data
         combo_bind(0)
 
     def combo_bind(event):
-        global combo_var, pages_data
+        """Do this when person selecte"""
+        global combo_var, pages_data, user_num
         print(combo_var.get())
+        text_geter_in_entry(user_num)
+
         for i, el in enumerate(pages_data):
             try:
                 a = combo_var.get()
-                if el['name'] == a.split(',')[0]:
+                if el['name'] == combo_var.get().split(',')[0]:
                     print('Выбранн элемент {}'.format(i))
-                    text_geter_in_entry(i)
-                    text_inserter_to_entry(i)
+                    user_num = i
+                    # text_inserter_to_entry(i)
             except TypeError:
                 pass
 
     def pages_data_to_insert_format(pages_data):
+        """переобразует pages_data в переменную удобную для записывания в combobox
+                    <name>,   <rub>.<cop> Руб
+        """
         tmp = []
         for i, el in enumerate(pages_data):
             if el is None:
@@ -119,9 +134,12 @@ def main(fp):
     my_pages_data = pages_data
 
     insert_pages_data = pages_data_to_insert_format(pages_data)
+
     combo = Combobox(win, textvariable=combo_var, values=insert_pages_data, width=70)
     combo.grid(row=0, columnspan=5, pady=3)
     combo.bind('<<ComboboxSelected>>', combo_bind)
+
+    # GUI
     global ename, eperiod, eaddress, ecost
     ename.grid(row=1, column=1)
     Label(text='ФИО').grid(row=1, column=0)
@@ -139,4 +157,5 @@ def main(fp):
 
 
 if __name__ == '__main__':
-    main("test.pdf")
+    # main("test.pdf")
+    print(main.__doc__)
